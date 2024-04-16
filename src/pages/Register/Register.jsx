99e29a319d4c86ac3842dcf0c12cd4adf193180d
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-04-16 08:59:34 +0600

    made necessary changes

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+
+const Register = () => {
+    const {createUser} = useContext(AuthContext);
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        // console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        const email = form.get('email');
+        const password = form.get('password');
+        const name = form.get('name');
+        const photo = form.get('photo');
+        console.log(email, password, name, photo);
+
+        // Create User
+        createUser(email, password)
+        .then(result =>{
+            console.log(result.user)
+        })
+        .catch(error =>{
+            console.error(error)
+        })
+
+    }
+
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                <h2 className="text-center text-3xl my-10">Please Register</h2>
+
+                <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto">
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input name="name" type="text" placeholder="Name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input name="photo" type="text" placeholder="Photo URL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input name="email" type="email" placeholder="Email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input name="password" type="password" placeholder="Password" className="input input-bordered" required />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                        </div>
+                        <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+                <p className="text-center mt-4">Already have an account? 
+                    <Link className="text-blue-600 font-semibold" to="/login">Login here!</Link>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default Register;
--- src/pages/Shared/Navbar/Navbar.jsx
+++ src/pages/Shared/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
 import { Link, NavLink } from "react-router-dom";
 import userDefaultPic from '../../../assets/user.png';
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const Navbar = () => {
+
+    const {user, logOut} = useContext(AuthContext);
+    
+    const handleSignOut = ()=>{
+        logOut()
+        .then(()=>{
+
+        })
+        .catch(error=>{
+            console.error(error);
+        })
+        
+    }
 
     const navLinks = <>
         <li><NavLink to="/">Home</NavLink></li>
@@ -35,9 +50,14 @@ const Navbar = () => {
                         <img src={userDefaultPic} />
                     </div>
                 </label>
-                <Link to='/login'>
-                    <button className="btn">Login</button>
-                </Link>
+                {
+                    user ? 
+                    <button onClick={handleSignOut} className="btn">Sign Out</button>
+                    :
+                    <Link to='/login'>
+                        <button className="btn">Login</button>
+                    </Link>
+                }
             </div>
         </div>
     );
